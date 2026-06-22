7b30e4ced9063c4f1f7a8837538f03a17874a365
sudeepbogati7 <sudeepbogati977@gmail.com>
2026-06-23 00:55:46 +0545

    overall page design push

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import Marquee from "@/components/Marquee";
 import About from "@/components/About";
 import TextBreak from "@/components/TextBreak";
 import ServicesStory from "@/components/ServicesStory";
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <>
       <Hero />
+      <Marquee />
       <About />
       <TextBreak
         lines={["Most software adds complexity.", "<em>We remove it.</em>"]}
